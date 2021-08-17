3cf846e1b6a9428dc16474ebf88291d3bfb5662c
Epic91 <hernandeze791@gmail.com>
2021-08-17 10:09:25 -0500

    migrated the db

---
+++ db/migrate/20210817150643_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.string :task
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
